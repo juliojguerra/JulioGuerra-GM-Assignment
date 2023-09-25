15c17c64ef2eca692f4990ab6e9ab69862046fed
Julio Guerra <j.guerratamayo@gmail.com>
2023-09-25 02:18:27 -0500

    Add navigateTo Widget and Interactions in Homepage

--- page-objects/HomePage.ts
+++ page-objects/HomePage.ts
@@ -1,16 +1,24 @@
 import { Locator, Page } from "@playwright/test";
 import ElementsPage from "./ElementsPage";
 import FormsPage from "./FormsPage";
+import WidgetsPage from "./WidgetsPage";
+import InteractionsPage from "./InteractionsPage";
 
 class HomePage {
   page: Page;
   elementsHeading: Locator;
   formsHeading: Locator;
+  widgetsHeading: Locator;
+  interactionsHeading: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.elementsHeading = this.page.getByRole("heading", { name: "Elements" });
     this.formsHeading = this.page.getByRole("heading", { name: "Forms" });
+    this.widgetsHeading = this.page.getByRole("heading", { name: "Widgets" });
+    this.interactionsHeading = this.page.getByRole("heading", {
+      name: "Interactions",
+    });
   }
 
   async go() {
@@ -25,6 +33,16 @@ class HomePage {
   async navigateToForms() {
     await this.formsHeading.click();
     return new FormsPage(this.page);
+  }
+
+  async navigateToWidget() {
+    await this.widgetsHeading.click();
+    return new WidgetsPage(this.page);
+  }
+
+  async navigateToInteractions() {
+    await this.interactionsHeading.click();
+    return new InteractionsPage(this.page);
   }
 }
 
